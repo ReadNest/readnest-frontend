56a40cb9522c9e2379762667d04e5ffa974ac708
21anhn <ltnanh21@gmail.com>
2025-07-03 19:26:56 +0700

    [Update] Update header UI with role admin

--- ReadNest/src/components/layout/Header.tsx
+++ ReadNest/src/components/layout/Header.tsx
@@ -4,13 +4,15 @@ import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { Button, buttonVariants } from "../ui/button";
 import { useState } from "react";
 import type { VariantProps } from "class-variance-authority";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "@/features/auth/authSlice";
 import UserDropDown from "../ui/user-dropdown";
 import { clearErrors } from "@/store/error/errorSlice";
 import { toast } from "react-toastify";
 import SearchContainer from "@/features/search/components/SearchContainer";
 import { ROUTE_PATHS } from "@/constants/routePaths";
+import type { RootState } from "@/store";
+import { RoleEnum } from "@/constants/enum";
 
 interface HeaderProps {
   isAuthenticated: boolean;
@@ -51,6 +53,9 @@ export const Header = ({ isAuthenticated, user }: HeaderProps) => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const isAdmin = useSelector(
+    (state: RootState) => state.auth.user.roleName === RoleEnum.ADMIN
+  );
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen((prev) => !prev);
@@ -88,33 +93,38 @@ export const Header = ({ isAuthenticated, user }: HeaderProps) => {
 
   return (
     <header className="w-full px-6 py-4 shadow-md bg-white flex items-center justify-between z-20 relative">
-      <div
-        className="flex items-center gap-2 hover:cursor-pointer"
-        onClick={() => {
-          navigate(ROUTE_PATHS.DEFAULT);
-        }}
-      >
-        <img src={readnestLogo} alt="Logo" className="w-8 h-8" />
-        <span className="text-xl font-bold">ReadNest</span>
-      </div>
-      <nav className="hidden md:flex gap-8 text-sm font-medium mr-auto ml-20">
-        {navItems.map(({ to, label, icon }) => (
-          <NavLink
-            key={to}
-            to={to}
-            className={({ isActive }) =>
-              `flex items-center gap-1 ${
-                isActive
-                  ? "text-indigo-700"
-                  : "text-gray-600 hover:text-indigo-600"
-              }`
-            }
+      {/* Ẩn logo và nav nếu là admin */}
+      {!isAdmin && (
+        <>
+          <div
+            className="flex items-center gap-2 hover:cursor-pointer"
+            onClick={() => {
+              navigate(ROUTE_PATHS.DEFAULT);
+            }}
           >
-            {icon}
-            {label}
-          </NavLink>
-        ))}
-      </nav>
+            <img src={readnestLogo} alt="Logo" className="w-8 h-8" />
+            <span className="text-xl font-bold">ReadNest</span>
+          </div>
+          <nav className="hidden md:flex gap-8 text-sm font-medium mr-auto ml-20">
+            {navItems.map(({ to, label, icon }) => (
+              <NavLink
+                key={to}
+                to={to}
+                className={({ isActive }) =>
+                  `flex items-center gap-1 ${
+                    isActive
+                      ? "text-indigo-700"
+                      : "text-gray-600 hover:text-indigo-600"
+                  }`
+                }
+              >
+                {icon}
+                {label}
+              </NavLink>
+            ))}
+          </nav>
+        </>
+      )}
       <button
         className="md:hidden flex items-center justify-center p-2 text-indigo-600 focus:outline-none"
         aria-label="Toggle menu"
@@ -153,30 +163,43 @@ export const Header = ({ isAuthenticated, user }: HeaderProps) => {
         )}
       </button>
       {isMobileMenuOpen && (
-        <div
-          className="fixed top-16 left-0 w-full h-[calc(100vh-4rem)] bg-white shadow-lg p-6 flex flex-col gap-6 md:hidden overflow-y-auto z-30"
-          // top-16 = 64px (header height)
-        >
-          {navItems.map(({ to, label, icon }) => (
-            <NavLink
-              key={to}
-              to={to}
-              className={({ isActive }) =>
-                `flex items-center gap-3 text-xl font-semibold px-4 py-3 rounded-lg transition-colors ${
-                  isActive
-                    ? "bg-indigo-100 text-indigo-700"
-                    : "text-gray-800 hover:bg-indigo-50"
-                }`
-              }
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              {icon}
-              {label}
-            </NavLink>
-          ))}
-
+        <div className="fixed top-16 left-0 w-full h-[calc(100vh-4rem)] bg-white shadow-lg p-6 flex flex-col gap-6 md:hidden overflow-y-auto z-30">
+          {/* Nav items hidden for admin */}
+          {!isAdmin &&
+            navItems.map(({ to, label, icon }) => (
+              <NavLink
+                key={to}
+                to={to}
+                className={({ isActive }) =>
+                  `flex items-center gap-3 text-xl font-semibold px-4 py-3 rounded-lg transition-colors ${
+                    isActive
+                      ? "bg-indigo-100 text-indigo-700"
+                      : "text-gray-800 hover:bg-indigo-50"
+                  }`
+                }
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                {icon}
+                {label}
+              </NavLink>
+            ))}
           <div className="mt-auto flex flex-col gap-3 px-4">
-            {!isAuthenticated ? (
+            {isAdmin ? (
+              isAuthenticated && (
+                <div className="mt-3">
+                  <UserDropDown
+                    username={user?.username ?? ""}
+                    fullName={user?.fullName ?? ""}
+                    avatarUrl={user?.avatarUrl}
+                    onClickLogout={() => {
+                      onLogout();
+                      setMobileMenuOpen(false);
+                    }}
+                    onlyLogout
+                  />
+                </div>
+              )
+            ) : !isAuthenticated ? (
               <>
                 <Link to="/login" onClick={() => setMobileMenuOpen(false)}>
                   <Button
@@ -235,11 +258,28 @@ export const Header = ({ isAuthenticated, user }: HeaderProps) => {
           </div>
         </div>
       )}
-      {/* Right side: search + user actions */}
-      <div className="flex items-center gap-4 ml-4">
-        <SearchContainer />
-
-        {!isAuthenticated ? (
+      <div className="flex items-center gap-4 ml-auto">
+        {!isAdmin && <SearchContainer />}
+        {isAdmin ? (
+          isAuthenticated && (
+            <>
+              <button
+                type="button"
+                className="flex justify-center"
+                onClick={() => toast.info("Tính năng đang được phát triển!")}
+              >
+                <Bell className="h-5 w-5 hover:animate-shake transition-transform cursor-pointer text-indigo-600" />
+              </button>
+              <UserDropDown
+                username={user?.username ?? ""}
+                fullName={user?.fullName ?? ""}
+                avatarUrl={user?.avatarUrl}
+                onClickLogout={onLogout}
+                onlyLogout
+              />
+            </>
+          )
+        ) : !isAuthenticated ? (
           <>
             <Link to="/login" className="w-24">
               <Button {...getButtonProps("login", location.pathname)}>
@@ -262,9 +302,13 @@ export const Header = ({ isAuthenticated, user }: HeaderProps) => {
                 Tạo bài viết
               </Button>
             </Link>
-            <Link to="/notifications">
-              <Bell className="h-5 w-5 hover:animate-shake transition-transform cursor-pointer" />
-            </Link>
+            <button
+              type="button"
+              className="flex justify-center"
+              onClick={() => toast.info("Tính năng đang được phát triển!")}
+            >
+              <Bell className="h-5 w-5 hover:animate-shake transition-transform cursor-pointer text-indigo-600" />
+            </button>
             <UserDropDown
               username={user?.username ?? ""}
               fullName={user?.fullName ?? ""}
